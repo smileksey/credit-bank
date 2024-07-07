e31e475c1bd75eb81f5be747d9fbe98738cff523
smileksey <smileksey@gmail.com>
2024-07-07 16:23:02 +0300

    Add checks to KafkaProducer's send methods; refactor DealClient's URL string concatenation

--- deal/src/main/java/org/smileksey/deal/services/KafkaProducer.java
+++ deal/src/main/java/org/smileksey/deal/services/KafkaProducer.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.smileksey.deal.dto.EmailMessage;
 import org.smileksey.deal.dto.EmailMessageWithSES;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
 @Slf4j
@@ -14,34 +17,108 @@ public class KafkaProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    //TODO add checking the result of sending
+
     public void sendFinishRegistrationMessage(EmailMessage emailMessage) {
-        kafkaTemplate.send("finish-registration", emailMessage);
-        log.info("Sent message = [{}] | topic = [finish-registration]", emailMessage);
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send("finish-registration", emailMessage);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("Sent message = [{}] | topic = [finish-registration]", emailMessage);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message = [{}] | topic = [finish-registration]", emailMessage);
+            }
+        });
     }
+
 
     public void sendCreateDocumentsMessage(EmailMessage emailMessage) {
-        kafkaTemplate.send("create-documents", emailMessage);
-        log.info("Sent message = [{}] | topic = [create-documents]", emailMessage);
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send("create-documents", emailMessage);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("Sent message = [{}] | topic = [create-documents]", emailMessage);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message = [{}] | topic = [create-documents]", emailMessage);
+            }
+        });
     }
+
 
     public void sendSendDocumentsMessage(EmailMessage emailMessage) {
-        kafkaTemplate.send("send-documents", emailMessage);
-        log.info("Sent message = [{}] | topic = [send-documents]", emailMessage);
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send("send-documents", emailMessage);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("Sent message = [{}] | topic = [send-documents]", emailMessage);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message = [{}] | topic = [send-documents]", emailMessage);
+            }
+        });
+
     }
+
 
     public void sendSendSESMessage(EmailMessageWithSES emailMessage) {
-        kafkaTemplate.send("send-ses", emailMessage);
-        log.info("Sent message = [{}] | topic = [send-ses]", emailMessage);
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send("send-ses", emailMessage);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("Sent message = [{}] | topic = [send-ses]", emailMessage);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message = [{}] | topic = [send-ses]", emailMessage);
+            }
+        });
+
     }
+
 
     public void sendCreditIssuedMessage(EmailMessage emailMessage) {
-        kafkaTemplate.send("credit-issued", emailMessage);
-        log.info("Sent message = [{}] | topic = [credit-issued]", emailMessage);
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send("credit-issued", emailMessage);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("Sent message = [{}] | topic = [credit-issued]", emailMessage);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message = [{}] | topic = [credit-issued]", emailMessage);
+            }
+        });
     }
 
+
     public void sendStatementDeniedMessage(EmailMessage emailMessage) {
-        kafkaTemplate.send("statement-denied", emailMessage);
-        log.info("Sent message = [{}] | topic = [statement-denied]", emailMessage);
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send("statement-denied", emailMessage);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("Sent message = [{}] | topic = [statement-denied]", emailMessage);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message = [{}] | topic = [statement-denied]", emailMessage);
+            }
+        });
+
     }
 }
--- dossier/src/main/java/org/smileksey/dossier/services/DealClient.java
+++ dossier/src/main/java/org/smileksey/dossier/services/DealClient.java
@@ -21,17 +21,22 @@ public class DealClient {
     @Value("${deal.url}")
     private String dealUrl;
 
+    /** Path to the '/admin/statement/{statementId}/status' endpoint */
+    private static final String UPDATE_STATUS_PATH = "/deal/admin/statement/{statementId}/status";
+
 
     /**
      * Method executes a PUT request to the 'deal' microservice to update a Statement entity
      * @param statementId - ID of the Statement entity to be updated
      */
     public void sendUpdateStatementStatus(UUID statementId) {
-        String url = dealUrl + "/deal/admin/statement/" + statementId + "/status";
+        String url = dealUrl + UPDATE_STATUS_PATH.replace("{statementId}", statementId.toString());
+
         restTemplate.exchange(
                 url,
                 HttpMethod.PUT, HttpEntityConstructor.createHttpEntity(null),
                 Void.class);
+
         log.info("Sending PUT request to {}", url);
     }
 }
